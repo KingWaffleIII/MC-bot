5eeb9f0b08b8223aacfd9245af61cbe34136ac2f
KingWaffleIII <waffle@planetwaffle.net>
2023-09-03 17:07:44 +0100

    remove event loading

--- src/bot.ts
+++ src/bot.ts
@@ -63,20 +63,20 @@ for (const file of commandFiles) {
 	}
 }
 
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs
-	.readdirSync(eventsPath)
-	.filter((file) => file.endsWith(".js"));
+// const eventsPath = path.join(__dirname, "events");
+// const eventFiles = fs
+// 	.readdirSync(eventsPath)
+// 	.filter((file) => file.endsWith(".js"));
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = (await import(filePath)).default;
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
+// for (const file of eventFiles) {
+// 	const filePath = path.join(eventsPath, file);
+// 	const event = (await import(filePath)).default;
+// 	if (event.once) {
+// 		client.once(event.name, (...args) => event.execute(...args));
+// 	} else {
+// 		client.on(event.name, (...args) => event.execute(...args));
+// 	}
+// }
 
 client.on(Events.ClientReady, async (bot) => {
 	console.log(`Bot is ready, logged in as ${bot.user.tag}!`);
